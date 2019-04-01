4d703b5eed7981aefda82fff23491cbbdf7ecbd5
Andreas Fink <andreas.fink@cscs.ch>
2019-04-01 14:19:24 +0200

    *update* default ycm config file updated

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -12,20 +12,8 @@ flags = [
     '-Wno-long-long',
     '-Wno-variadic-macros',
     '-fexceptions',
-    # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-    # language to use when compiling headers. So it will guess. Badly. So C++
-    # headers will be compiled as C headers. You don't want that so ALWAYS specify
-    # a "-std=<something>".
-    # For a C project, you would set this to something like 'c99' instead of
-    # 'c++11'.
     '-std=c++17',
-    # ...and the same thing goes for the magic -x option which specifies the
-    # language that the files to be compiled are written in. This is mostly
-    # relevant for c++ headers.
-    # For a C project, you would set this to 'c' instead of 'c++'.
     '-x', 'c++',
-    # This path will only work on OS X, but extra paths that don't exist are not
-    # harmful
     '-isystem', '/usr/include/glib-2.0',
     '-isystem', '/usr/lib64/glib-2.0/include',
     '-isystem', '/usr/include/libsecret-1',
